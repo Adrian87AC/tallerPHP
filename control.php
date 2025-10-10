35a775f7ad7d4395d15fd60cf39fa9f814f54601
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-10-10 09:52:36 +0200

    cambios

---
+++ control.php
@@ -0,0 +1,10 @@
+<?php
+$numero = 78;
+
+if($numero > 0){
+    echo "El número es positivo";
+}else if($numero < 0){
+    echo "El número es negativo";
+}else if($numero == 0){
+    echo "El número es cero";
+}
