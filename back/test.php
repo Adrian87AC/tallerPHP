abf06b61515f86547f27ab516579ab763d5b495f
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-10-27 10:02:26 +0100

    más cambios

--- back/test.php
+++ back/test.php
@@ -2,14 +2,14 @@
     require __DIR__ .'/interfaces/ianimal.php';
     require __DIR__ .'/class/perro.php';
     require __DIR__ . '/class/gato.php';
-    
+
     require __DIR__ . '/interfaces/ivehiculo.php';
     require __DIR__ . '/class/abstractTransporte.php';
     require __DIR__ . '/class/bicicleta.php';
     
     $oPerro = new Perro();
     $oPerro->hacerSonido();
-
+    
     $oGato = new Gato();
     $oGato->hacerSonido();
 
